ccc2a5404eabfca2fef3a1ecffc2b02d634a9cfa
gaoweijian <gaoweijiancoming@163.com>
2026-05-22 20:07:06 +0800

    [高伟健]增加富文本功能

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,11 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.addViewController("/admin").setViewName("forward:/admin.html");
         registry.addViewController("/books").setViewName("forward:/books.html");
+        registry.addViewController("/richtext").setViewName("forward:/richtext.html");
     }
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache());
     }
 }
